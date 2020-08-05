70b2cf0bc5181272802a777dfe0f3f32ada0ba07
István Palásthy <istvanpalasthy@yahoo.com>
2020-08-05 11:45:05 +0200

    elso commit

---
+++ src/main/java/com/codecool/NorthwindPractice/NorthwindPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.NorthwindPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/NorthwindPractice/NorthwindPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.NorthwindPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
